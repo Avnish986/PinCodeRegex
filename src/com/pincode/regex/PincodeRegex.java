0b9d15df313bb228c57d6fecad7255238081834e
avnish986 <avnishgupta986@gmail.com>
2020-09-28 17:34:10 +0530

    [Avnish]Created:Pincode validation

---
+++ src/com/pincode/regex/PincodeRegex.java
@@ -0,0 +1,21 @@
+package com.pincode.regex;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class PincodeRegex {
+
+	public static void main(String[] args) {
+		Pattern patt = Pattern.compile("^[0-9]{3}\\s{0,1}[0-9]{3}$");
+		Scanner sc = new Scanner(System.in);
+		String pinCode = sc.nextLine();
+		Matcher matcher = patt.matcher(pinCode); //use 12345 matches starts and end with num
+		boolean found=matcher.find();
+		if(found)
+			System.out.println("Valid");
+		else
+			System.out.println("Invalid");
+	
+
+	}
+
+}
